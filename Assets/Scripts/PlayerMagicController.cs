6c5dc889a43f887b8353fe4870c0106df5e8c68d
PyroPoro <rickywen12345@gmail.com>
2024-01-20 14:11:54 -0500

    Added projectile spawning
    spawning test projectile on left click

--- Assets/Scripts/PlayerMagicController.cs
+++ Assets/Scripts/PlayerMagicController.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMagicController : MonoBehaviour
 {
+    public GameObject projectile;
+    public GameObject player;
+    public GameObject playerCam;
+    [SerializeField]
+    private LayerMask layermask;
+    private Vector3 difference;
+    private Vector3 direction;
     void Start()
     {
         
@@ -11,6 +19,24 @@ public class PlayerMagicController : MonoBehaviour
 
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            ShootProjectile(projectile);
+        }
+    }
+
+    void ShootProjectile(GameObject projectileToShoot) {
+        direction = new Vector2(0,0);
+        GameObject spawnedProjectile = Instantiate(projectileToShoot, player.transform.position, Quaternion.identity);
+        Ray ray = playerCam.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction, out RaycastHit hit, Mathf.Infinity, layermask))
+        {
+            difference = hit.point - player.transform.position;
+            direction = new Vector3(difference.x, 0, difference.z).normalized;
+        }
+        float projectileSpeed = projectileToShoot.GetComponent<Projectile>().speed;
+        float projectileLifeTime = projectileToShoot.GetComponent<Projectile>().lifeTime;
+        spawnedProjectile.GetComponent<Rigidbody>().velocity = direction * projectileSpeed;
+        spawnedProjectile.GetComponent<Projectile>().player = gameObject;
+        Destroy(spawnedProjectile, projectileLifeTime);
     }
 }
