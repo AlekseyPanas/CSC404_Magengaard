fe9df815e86cb8b3ef2337b133f5c6c404396b8a
PyroPoro <rickywen12345@gmail.com>
2024-01-25 21:59:17 -0500

    Added fireball prefab
    Fireball script with no art assets
    explodes on projectile death, i.e reaches the end of its path or collides with enemy.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -10,11 +10,20 @@ public class Projectile: MonoBehaviour
     public float lifeTime;
     public GameObject player;
     public float damage;
+    [SerializeField] private GameObject explosion_prefab;
 
     void OnTriggerEnter(Collider col){
         if (col.gameObject.CompareTag("Player") && col.gameObject != player){
             col.gameObject.GetComponent<PlayerCombatManager>().TakeDamage((int)damage);
             Destroy(gameObject);
+        }
+    }
+
+    void OnDestroy(){
+        if(explosion_prefab != null){
+            GameObject explosion = Instantiate(explosion_prefab,transform.position,Quaternion.identity);
+            explosion.GetComponent<Projectile>().player = player;
+            Destroy(explosion, explosion.GetComponent<Projectile>().lifeTime);
         }
     }
 }
---
+++ Assets/Scripts/Spells/Fireball/FireballController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
