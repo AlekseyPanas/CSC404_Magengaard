93fb51f0908366b70c07d7465367283ca3ab78bf
AlekseyPanas <panasaleksey3@gmail.com>
2024-02-08 23:34:53 -0500

    Add wind effect

---
+++ Assets/Scripts/Effects/WindEffect.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+/** 
+* Effect for when an object is being blown by wind
+*/
+class WindEffect {
+    public Vector3 Velocity {get; private set;}  // Encodes wind direction and speed as relevant to target object
+
+    public WindEffect setWindVelocity(Vector3 vel) {
+        Velocity = vel;
+        return this;
+    }
+}
