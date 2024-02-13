80d73c7ac21f9c20a4becb57948680b87528bcb6
AlekseyPanas <panasaleksey3@gmail.com>
2024-02-13 14:52:18 -0500

    Start cutscene work

---
+++ Assets/Prefabs/MenuSequence/Cutscene/CutsceneImageData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneImageData : MonoBehaviour
+{
+    [Tooltip("Where the image moves over the course of the cutscene duration")][SerializeField] public Vector2 displacement;
+}
---
+++ Assets/Prefabs/MenuSequence/Cutscene/CutsceneManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CutsceneManager : MonoBehaviour
+{
+    [SerializeField] private FadeToBlackPanel fadeToBlackPanel;
+    [SerializeField] private CutsceneData[] cutscenes;
+    [SerializeField] private TextMeshProUGUI subtitles;
+    
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
