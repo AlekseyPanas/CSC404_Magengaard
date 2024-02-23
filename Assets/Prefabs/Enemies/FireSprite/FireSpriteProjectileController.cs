cb9d55558591fd40e30284327d1f58792010ac58
PyroPoro <rickywen12345@gmail.com>
2024-02-22 21:56:20 -0500

    idk why these didnt push

---
+++ Assets/Prefabs/Enemies/FireSprite/FireSpriteProjectileController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class FireSpriteProjectileController : NetworkBehaviour
+{
+    [SerializeField] float lifetime;
+    [SerializeField] float timer;
+    [SerializeField] float damageInterval;
+    [SerializeField] float damage;
+    [SerializeField] List<GameObject> currentlyColliding;
+    public GameObject parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+        timer = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > timer){
+            timer = Time.time + damageInterval;
+            DoDamage();
+        }
+        transform.position = parent.transform.position;
+        transform.forward = parent.transform.forward;
+    }
+
+    void DoDamage(){
+        if (currentlyColliding.Count > 0) {
+            IEffectListener<DamageEffect>.SendEffect(currentlyColliding, new DamageEffect{Amount = (int)damage});
+        }
+    }
+
+    void OnTriggerEnter(Collider col){
+        if (col.CompareTag("Player")){
+            if (!currentlyColliding.Contains(col.gameObject)){
+                currentlyColliding.Add(col.gameObject);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider col){
+        if (col.CompareTag("Player")){
+            if (currentlyColliding.Contains(col.gameObject)){
+                currentlyColliding.Remove(col.gameObject);
+            }
+        }
+    }
+}
