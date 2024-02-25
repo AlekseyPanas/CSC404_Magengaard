1dbbc8245e3be9968f45048b8d97d99c9fd84043
PyroPoro <rickywen12345@gmail.com>
2024-02-25 18:46:07 -0500

    ruins door prefab and animations

---
+++ Assets/Prefabs/InteractableObjects/RuinsDoor/RuinsDoorController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class RuinsDoorController : NetworkBehaviour, IEffectListener<WindEffect>
+{
+    [SerializeField] Animator anim;
+    public void OnEffect(WindEffect effect)
+    {
+        anim.SetTrigger("Open");
+    }
+}
