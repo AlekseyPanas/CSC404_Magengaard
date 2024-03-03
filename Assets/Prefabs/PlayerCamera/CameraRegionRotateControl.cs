eae470f89be95348824a45f489e9c2afea542dfb
AlekseyPanas <panasaleksey3@gmail.com>
2024-03-03 18:05:11 -0500

    Add ruins, add rotate-able testing camera

---
+++ Assets/Prefabs/PlayerCamera/CameraRegionRotateControl.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+/**
+ * On player enter, the camera follows the player from some offset away.
+ */
+public class CameraRegionRotateControls : MonoBehaviour
+{
+    /**
+     * Starting position of the camera. Set to some empty child.
+     */
+    public float offset;
+    /**
+     * Glide speed, every frame this the origin and destination camera are Lerp'd with this value.
+     */
+    public float speed = 0.1f;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        var current = Camera.main;
+
+        if (current == null) { return; }
+        
+        var manager = current.GetComponent<CameraManager>();
+
+        if (manager == null) { return; }
+        
+        manager.SwitchFollow(this, new CameraFollowRotateControls(offset, speed));
+    }
+}
