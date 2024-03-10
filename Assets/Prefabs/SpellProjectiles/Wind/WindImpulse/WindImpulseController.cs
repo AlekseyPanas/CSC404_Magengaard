957195ffa7bfee6e944ce1a059d1ba3dd18d6672
PyroPoro <rickywen12345@gmail.com>
2024-03-09 23:24:43 -0500

    wind impulse update with bins

--- Assets/Prefabs/SpellProjectiles/Wind/WindImpulse/WindImpulseController.cs
+++ Assets/Prefabs/SpellProjectiles/Wind/WindImpulse/WindImpulseController.cs
@@ -5,18 +5,24 @@ using Unity.Netcode;
 
 public class WindImpulseController : NetworkBehaviour, ISpell
 {
+    [SerializeField] private float _baseWindEffectSpeed;
+    [SerializeField] float windEffectSpeed;
+    [SerializeField] private float _baseDamage;
+    [SerializeField] float damage;
     [SerializeField] private float lifeTime;
+    [SerializeField] int _spellStrength = 4;
     public GameObject player;
-    [SerializeField] private float damage;
     private Vector3 dir;
     public ulong playerID;
-    [SerializeField] private float windEffectSpeed;
-    float timer = 0.1f;
+    float timer;
     List<GameObject> objectsAlreadyCollided;
+    
+    SpellParamsContainer spellParams;
     void Awake(){
-        Invoke("DestroySpell", lifeTime);
-        timer += Time.time;
+        Invoke(nameof(DestroySpell), lifeTime);
+        timer = 0.1f + Time.time;
         objectsAlreadyCollided = new List<GameObject>();
+        GetComponent<Collider>().enabled = true;
     }
 
     void OnTriggerEnter(Collider col){
@@ -40,6 +46,7 @@ public class WindImpulseController : NetworkBehaviour, ISpell
     public void preInitBackfire() { }
 
     public void preInit(SpellParamsContainer spellParams) {
+        this.spellParams = spellParams;
         player = NetworkManager.Singleton.ConnectedClients[playerID].PlayerObject.gameObject;
         transform.position = player.transform.position;
 
@@ -51,12 +58,50 @@ public class WindImpulseController : NetworkBehaviour, ISpell
     public void postInit() {
         Vector3.Normalize(dir);
         dir = new Vector3(dir.x, 0, dir.z);
+
+        ApplySpellStrength();
+
         transform.position = player.transform.position + dir * Const.SPELL_SPAWN_DISTANCE_FROM_PLAYER; //second number in the vector should be around the height of the player's waist
+        transform.forward = dir;
     }
 
     public void Update(){
         if (Time.time > timer){
             GetComponent<Collider>().enabled = false;
+        }
+    }
+
+    void ApplySpellStrength(){
+        float multiplier = 0.8f + _spellStrength * 0.2f;
+        damage = _baseDamage * multiplier;
+        windEffectSpeed = _baseWindEffectSpeed * multiplier;
+        transform.localScale *= multiplier;
+        if (_spellStrength == 3) {
+            StartCoroutine(SpawnClusterAfterDelay(2));
+        }
+        if (_spellStrength == 4) {
+            StartCoroutine(SpawnClusterAfterDelay(4));
+        }
+    }
+    public void SetSpellStrength(int spellStrength){
+        _spellStrength = spellStrength;
+    }
+    void SpawnCluster(int i){
+        GameObject cluster = Instantiate(gameObject, transform.position, Quaternion.identity);
+        ISpell iSpell = cluster.GetComponent<ISpell>();
+        iSpell.setPlayerId(playerID);
+        iSpell.preInit(spellParams);
+        cluster.GetComponent<NetworkObject>().Spawn();
+        cluster.GetComponent<WindImpulseController>().SetSpellStrength(1);
+        iSpell.postInit();
+        cluster.transform.position = transform.position + ((i + 1) * 0.3f * transform.forward) + ((i % 2 * 2 - 1) * 0.2f * transform.right);
+        cluster.transform.localScale *= 1 - i * 0.1f;
+    }
+
+    IEnumerator SpawnClusterAfterDelay(int numClusters){
+        for(int i = 0; i < numClusters; i++) {
+            yield return new WaitForSeconds(0.15f);
+            SpawnCluster(i);
         }
     }
 }
