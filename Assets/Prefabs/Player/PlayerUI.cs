caa7ae14d78580970ab02f5b1ac1ed73c7703b85
PyroPoro <rickywen12345@gmail.com>
2024-03-10 01:42:04 -0500

    added UI for spell energy

--- Assets/Prefabs/Player/PlayerUI.cs
+++ Assets/Prefabs/Player/PlayerUI.cs
@@ -8,13 +8,18 @@ public class PlayerUI : MonoBehaviour
     float hpPercent;
     [SerializeField] Image hpFill;
     [SerializeField] GameObject fadeToBlack;
+    [SerializeField] List<Transform> energySegments;
+    [SerializeField] List<GameObject> energyLevels;
+    [SerializeField] Image energyTimerBar;
+    public int totalSegments;
+    public int currSegments;
+    Camera energyCam;
 
     void Start(){
         PlayerHealthSystem.onTakedamage += UpdateHPBar;
         PlayerHealthSystem.onDeath += OnDeath;
         PlayerHealthSystem.onRespawn += FadeInFromBlack;
         PlayerHealthSystem.onRespawn += ResetHPBar;
-        
         fadeToBlack.SetActive(true);
     }
 
@@ -39,4 +44,44 @@ public class PlayerUI : MonoBehaviour
         hpFill.fillAmount = 1f;
     }
     
+    /*
+    Update energy levels, subscribe to the GestureSequenceAdd event
+    */
+    void OnNewGesture(Gesture g, int energyLevel, int segmentCount){
+
+        for(int i = 0; i < segmentCount; i++){
+            Instantiate(energyLevels[energyLevel], energySegments[i]);
+        }
+        currSegments = segmentCount;
+        //figure out how to draw the gesture into the circle
+    }
+
+    void OnEnergyChange(int segmentsLeft){
+        for (int i = currSegments; i > segmentsLeft; i--){
+            Destroy(energySegments[i-1].GetChild(0).gameObject);
+        }
+        currSegments = segmentsLeft;
+        //ResetRenderTexture();
+    }
+
+    void OnTimeBarChange(float fill){
+        energyTimerBar.fillAmount = fill;
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            OnNewGesture(new Gesture(), 2, 4);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2)){
+            OnEnergyChange(2);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha3)){
+            OnTimeBarChange(0.5f);
+        }
+    }
+
+    void ResetRenderTexture(){
+        RenderTexture targetTexture = energyCam.targetTexture;
+        energyCam.Render();
+    }
 }
