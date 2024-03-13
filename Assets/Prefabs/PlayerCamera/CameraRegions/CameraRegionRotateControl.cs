a564a52fa17d73d1fdc2605d3c4f04893a895664
Aleksey Panas <panasaleksey3@gmail.com>
2024-03-13 11:39:55 -0400

    Fix test cam controller

--- Assets/Prefabs/PlayerCamera/CameraRegions/CameraRegionRotateControl.cs
+++ Assets/Prefabs/PlayerCamera/CameraRegions/CameraRegionRotateControl.cs
@@ -1,29 +1,21 @@
-// using UnityEngine;
+using ACameraControllable = AControllable<CameraManager, ControllerRegistrant>;
+using UnityEngine;
 
-// /**
-//  * On player enter, the camera follows the player from some offset away.
-//  */
-// public class CameraRegionRotateControls : MonoBehaviour
-// {
-//     /**
-//      * Starting position of the camera. Set to some empty child.
-//      */
-//     public float offset;
-//     /**
-//      * Glide speed, every frame this the origin and destination camera are Lerp'd with this value.
-//      */
-//     public float speed = 0.1f;
+/**
+ * On player enter, the camera follows the player from some offset away.
+ */
+public class CameraRegionRotateControls : ACameraRegion
+{
+    /**
+     * Starting position of the camera. Set to some empty child.
+     */
+    public float offset;
+    /**
+     * Glide speed, every frame this the origin and destination camera are Lerp'd with this value.
+     */
+    public float speed = 0.1f;
     
-//     private void OnTriggerEnter(Collider other)
-//     {
-//         var current = Camera.main;
-
-//         if (current == null) { return; }
-        
-//         var manager = current.GetComponent<CameraManager>();
-
-//         if (manager == null) { return; }
-        
-//         manager.SwitchFollow(this, new CameraFollowRotateControls(offset, speed));
-//     }
-// }
+    protected override void OnTriggeredRegion(ACameraControllable manager, ControllerRegistrant controller, Collider player) {
+        manager.GetSystem(controller).SwitchFollow(controller, new CameraFollowRotateControls(offset, speed));
+    }
+}
