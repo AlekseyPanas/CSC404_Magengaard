833fe1e6cd5465499430267145ec1110d1443575
PyroPoro <rickywen12345@gmail.com>
2024-03-16 01:41:33 -0400

    Update AAggroProvider.cs

--- Assets/Prefabs/Enemies/AAggroProvider.cs
+++ Assets/Prefabs/Enemies/AAggroProvider.cs
@@ -6,8 +6,6 @@ public class AAggroProvider: MonoBehaviour
     public event Action<GameObject> AggroEvent;
 
     public void TriggerAggroEvent(GameObject g){
-        Debug.Log(AggroEvent == null);
-        Debug.Log("triggering agro event");
         AggroEvent?.Invoke(g);
     }
 }
