809914406f37e62cb6aac2e226c9d2f58c3f1e29
AlekseyPanas <panasaleksey3@gmail.com>
2024-03-17 23:00:19 -0400

    Implement sounds

--- Assets/Prefabs/InteractableObjects/Rubble/Rubble.cs
+++ Assets/Prefabs/InteractableObjects/Rubble/Rubble.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using FMODUnity;
 using UnityEngine;
 
 public class Rubble : MonoBehaviour
@@ -7,13 +8,17 @@ public class Rubble : MonoBehaviour
     private static readonly int ANIM_COLLAPSE = Animator.StringToHash("CollapseRubble");
 
     [SerializeField] private Animator _anim;
+    private StudioEventEmitter _emitter;
 
     // Start is called before the first frame update
     void Start() {
+        _emitter = GetComponent<StudioEventEmitter>();
+
         PickupSystem.OnTogglePickupSequence += (bool b) => { 
             if (!b && !_anim.GetBool(ANIM_COLLAPSE)) { 
                 _anim.SetBool(ANIM_COLLAPSE, true);
                 GetComponent<BoxCollider>().enabled = false;
+                _emitter.Play();
             }
         };
     }
