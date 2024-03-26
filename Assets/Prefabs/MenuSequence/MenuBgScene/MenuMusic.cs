29f1fcf0c966ffce5787afb112198c5b94b9b92d
AlekseyPanas <panasaleksey3@gmail.com>
2024-03-25 20:33:52 -0400

    Finalize menu music

--- Assets/Prefabs/MenuSequence/MenuBgScene/MenuMusic.cs
+++ Assets/Prefabs/MenuSequence/MenuBgScene/MenuMusic.cs
@@ -16,6 +16,6 @@ public class MenuMusic : MonoBehaviour {
     }
 
     void OnSceneChange(string nextScene) {
-        _emitter.Stop();
+        RuntimeManager.StudioSystem.setParameterByName("Out", 1);
     }
 }
